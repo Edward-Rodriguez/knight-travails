1c8aea8ec25506632bf7a4dc5cf65e4d9bd9ebbd
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2024-02-07 09:33:30 -0500

    Add buildGraph function

--- knightMoves.js
+++ knightMoves.js
@@ -1,24 +1,13 @@
-function knightMoves() {}
-
-function Node(row, col) {
-  const children = getEligibleSquares();
-
-  function getEligibleSquares() {
-    const eligibleSquares = [];
-    if (row - 2 >= 0 && col - 1 >= 0) eligibleSquares.push([row - 2, col - 1]);
-    if (row - 2 >= 0 && col + 1 >= 0) eligibleSquares.push([row - 2, col + 1]);
-    if (row - 1 >= 0 && col - 2 >= 0) eligibleSquares.push([row - 1, col - 2]);
-    if (row - 1 >= 0 && col + 2 >= 0) eligibleSquares.push([row - 1, col + 2]);
-    if (row + 1 >= 0 && col - 2 >= 0) eligibleSquares.push([row + 1, col - 2]);
-    if (row + 1 >= 0 && col + 2 >= 0) eligibleSquares.push([row + 1, col + 2]);
-    if (row + 2 >= 0 && col - 1 >= 0) eligibleSquares.push([row + 2, col - 1]);
-    if (row + 2 >= 0 && col + 1 >= 0) eligibleSquares.push([row + 2, col + 1]);
-    return eligibleSquares;
-  }
+function Node(position) {
+  const [row, col] = position;
+  let children = [];
 
   return {
     get children() {
       return children;
+    },
+    set children(arr) {
+      children = [...arr];
     },
     get row() {
       return row;
@@ -29,5 +18,54 @@ function Node(row, col) {
   };
 }
 
-const node = Node(3, 3);
-console.log(node.children);
+let buildGraph = () => {
+  const boardSize = 3;
+  const board = Array.from(Array(boardSize), () =>
+    new Array(boardSize).fill(null),
+  );
+  for (let row = 0; row < boardSize; row += 1) {
+    for (let col = 0; col < boardSize; col += 1) {
+      if (!board[row][col]) {
+        board[row][col] = Node([row, col]);
+      }
+      let node = board[row][col];
+      const possibleMoves = getPossibleMoves(row, col);
+      let childNodes = [];
+      possibleMoves.forEach((square) => {
+        const [squareRow, squareCol] = [...square];
+        if (!board[squareRow][squareCol]) {
+          board[squareRow][squareCol] = Node([squareRow, squareCol]);
+        }
+        childNodes.push(board[squareRow][squareCol]);
+      });
+      node.children = childNodes;
+    }
+  }
+
+  function getPossibleMoves(row, col) {
+    function withinRange(iRow, jCol) {
+      if (
+        iRow >= 0 &&
+        iRow <= boardSize - 1 &&
+        jCol >= 0 &&
+        jCol <= boardSize - 1
+      )
+        return true;
+      else return false;
+    }
+    const eligibleSquares = [];
+    if (withinRange(row - 2, col - 1)) eligibleSquares.push([row - 2, col - 1]);
+    if (withinRange(row - 2, col + 1)) eligibleSquares.push([row - 2, col + 1]);
+    if (withinRange(row - 1, col - 2)) eligibleSquares.push([row - 1, col - 2]);
+    if (withinRange(row - 1, col + 2)) eligibleSquares.push([row - 1, col + 2]);
+    if (withinRange(row + 1, col - 2)) eligibleSquares.push([row + 1, col - 2]);
+    if (withinRange(row + 1, col + 2)) eligibleSquares.push([row + 1, col + 2]);
+    if (withinRange(row + 2, col - 1)) eligibleSquares.push([row + 2, col - 1]);
+    if (withinRange(row + 2, col + 1)) eligibleSquares.push([row + 2, col + 1]);
+    return eligibleSquares;
+  }
+  return board[0][0]; //setting root node as bottom left square
+};
+
+function knightMoves() {}
+console.log(buildGraph());
